fb8fd139c4bbe908a94c165a3003bb39b1d76dd0
Drimkore <art.2000m.ab@gmail.com>
2023-05-05 13:41:01 +0500

    second change dataset

--- lab4/creation_features.py
+++ lab4/creation_features.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
-df_1 = df[['Make', 'Model', 'Year', 'Style', 'Price(euro)', 'km_year']]
-df_1.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
-
-
 #df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
-#counts = df.Make.value_counts() 
-#counts.median()
-#rare =  counts[(counts.values < 25)]
-#df['Make'] = df['Make'].replace(rare.index.values, 'Rare')
-#df.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
+#df_1 = df[['Make', 'Model', 'Year', 'Style', 'Price(euro)', 'km_year']]
+#df_1.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
+
+
+df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
+counts = df.Make.value_counts() 
+counts.median()
+rare =  counts[(counts.values < 25)]
+df['Make'] = df['Make'].replace(rare.index.values, 'Rare')
+df.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
 
 
 
