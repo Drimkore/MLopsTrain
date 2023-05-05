b84f13b4a7fb0d6e985f865c9e0c6c6f75921560
Drimkore <art.2000m.ab@gmail.com>
2023-05-05 13:42:47 +0500

    third (one hot encoding) change dataset

--- lab4/creation_features.py
+++ lab4/creation_features.py
@@ -6,15 +6,15 @@ import numpy as np
 #df_1.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
 
 
-df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
-counts = df.Make.value_counts() 
-counts.median()
-rare =  counts[(counts.values < 25)]
-df['Make'] = df['Make'].replace(rare.index.values, 'Rare')
-df.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
-
-
-
 #df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
-#hotEncoded = pd.get_dummies(df)
-#hotEncoded.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
+#counts = df.Make.value_counts() 
+#counts.median()
+#rare =  counts[(counts.values < 25)]
+#df['Make'] = df['Make'].replace(rare.index.values, 'Rare')
+#df.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
+
+
+
+df = pd.read_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', delimiter = ',')
+hotEncoded = pd.get_dummies(df)
+hotEncoded.to_csv('/home/art/gitproj/MLopsTrain/data/cars_moldova_clean.csv', index=False)
